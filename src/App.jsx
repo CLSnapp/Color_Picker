983347cf2967926881b64c746b519e1ed199c992
CLSnapp <repbandito@hotmail.com>
2024-11-12 20:06:31 -0600

    Color Picker Completed with Extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
-// Write your Color component here
+import { useState } from "react";
+
+// const Color = (props) => {
+//   return <div className={props.color}></div>;
+// };
+
+const Color = ({ color, setSelectedColor, selectedColor }) => {
+  return (
+    <div
+      className={`color-box ${color} ${
+        color === selectedColor ? "selected" : ""
+      }`}
+      onClick={() => setSelectedColor(color)}
+    ></div>
+  );
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={`selected-color-name ${selectedColor}`}>
+          {selectedColor}
+        </div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color
+          color="green"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+        <Color
+          color="violet"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+        <Color
+          color="yellow"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+      </div>
     </div>
   );
 };
